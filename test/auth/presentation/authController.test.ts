22903f1ae8c3fcfd98326e376f3ca7ea44774477
Ana Luiza Ramalho dos Santos <analuizaramalho9@gmail.com>
2023-03-09 20:49:58 -0300

    test: Ensure authController.signUp returns 400 if email is not valid

--- test/auth/presentation/authController.test.ts
+++ test/auth/presentation/authController.test.ts
@@ -1,8 +1,16 @@
-import { AuthController } from '../../../src/auth/presentation/authController'
+import { AuthController, type EmailValidatorInterface } from '../../../src/auth/presentation/authController'
+
+class EmailValidatorStub implements EmailValidatorInterface {
+  valid (email: string): boolean {
+    return true
+  }
+}
+
+const emailValidatorStub = new EmailValidatorStub()
 
 describe('SignUp', () => {
   test('Should return status 400 when name was not provided', () => {
-    const sut = new AuthController()
+    const sut = new AuthController(emailValidatorStub)
     const requestBody = {
       body: {
         email: 'test_email',
@@ -16,10 +24,26 @@ describe('SignUp', () => {
   })
 
   test('Should return status 400 when email was not provided', () => {
-    const sut = new AuthController()
+    const sut = new AuthController(emailValidatorStub)
     const requestBody = {
       body: {
         name: 'test_name',
+        password: 'test_password',
+        passwordConfirm: 'test_password'
+      }
+    }
+    const response = sut.signUp(requestBody)
+
+    expect(response.status).toBe(400)
+  })
+
+  test('Should return status 400 when email is not valid', () => {
+    jest.spyOn(emailValidatorStub, 'valid').mockReturnValueOnce(false)
+    const sut = new AuthController(emailValidatorStub)
+    const requestBody = {
+      body: {
+        name: 'test_name',
+        email: 'test_invalid_email',
         password: 'test_password',
         passwordConfirm: 'test_password'
       }

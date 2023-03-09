77b59c5b0e6f4cd01584da07f1e5d2b15a19618f
Ana Luiza Ramalho dos Santos <analuizaramalho9@gmail.com>
2023-03-09 20:38:36 -0300

    test: Ensure authController.signUp returns 400 if no email was provided

--- test/auth/presentation/authController.test.ts
+++ test/auth/presentation/authController.test.ts
@@ -14,4 +14,18 @@ describe('SignUp', () => {
 
     expect(response.status).toBe(400)
   })
+
+  test('Should return status 400 when email was not provided', () => {
+    const sut = new AuthController()
+    const requestBody = {
+      body: {
+        name: 'test_name',
+        password: 'test_password',
+        passwordConfirm: 'test_password'
+      }
+    }
+    const response = sut.signUp(requestBody)
+
+    expect(response.status).toBe(400)
+  })
 })
